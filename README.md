8029c2e2b3b552e7f30f504aeac925623d762a3c
labuschagnel01 <louwrens.labuschagne@vodacom.co.za>
2021-01-06 15:05:34 +0200

    bASICS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# infer-dot-net-basic-classifier
+# Basic Classifier using Infer.Net
+
+Inspired by the `one-feature` email classifier dicussed in <a href='https://mbmlbook.com/EmailClassifier_A_model_for_classification.html'>MBML</a> we'd like to build a simple classifier using infer.net.
+
+The factor graph dicussed in MBML looks like:
+
+<img src='assets/email-classifier.jpg' width='40%'>
---
+++ model/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.ML.Probabilistic.Models;
+using Microsoft.ML.Probabilistic.Compiler;
+using Algorithms =  Microsoft.ML.Probabilistic.Algorithms;
+
+namespace model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Variable<bool> firstCoin = Variable.Bernoulli(0.5).Named("firstCoin");
+            Variable<bool> secondCoin = Variable.Bernoulli(0.5).Named("secondCoin");
+            Variable<bool> bothHeads = (firstCoin & secondCoin).Named("bothHeads");
+            
+            InferenceEngine engine = new InferenceEngine();
+
+            if (engine.Algorithm is Algorithms.VariationalMessagePassing)
+            {
+                Console.WriteLine("This example does not run with Variational Message Passing");
+                return;
+            }
+            Console.WriteLine("Probability both coins are heads: " + engine.Infer(bothHeads));
+            bothHeads.ObservedValue = true; 
+            Console.WriteLine("Probability distribution over firstCoin: " + engine.Infer(firstCoin));
+
+            engine.ShowFactorGraph = true;
+            // engine.ShowMsl = true;
+        }
+    }
+}
