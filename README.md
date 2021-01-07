f672dbf3dddaca614d6fbad4e11433318cc924a8
labuschagnel01 <louwrens.labuschagne@vodacom.co.za>
2021-01-07 15:02:24 +0200

    Getting there, but no sigar yet..

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ The factor graph dicussed in MBML looks like:
 <img src='assets/email-classifier.jpg' width='40%'>
 
 with very objectified code <a href='https://github.com/dotnet/mbmlbook/blob/master/src/4.%20Uncluttering%20Your%20Inbox/Models/OneFeatureModel.cs'>here</a>.
+
+
+Our basic classifer will try and classify the setosa from the virginica irises from the Iris dataset, the distribution plot below shows this should be an easy task:
+
+<img src='./notebooks/sepal-length-dist.jpg' width='70%'>
--- model/Program.cs
+++ model/Program.cs
@@ -1,14 +1,77 @@
 ﻿using System;
+using Range = Microsoft.ML.Probabilistic.Models.Range;
 using Microsoft.ML.Probabilistic.Models;
+using Microsoft.ML.Probabilistic.Models.Attributes;
 using Microsoft.ML.Probabilistic.Compiler;
 using Algorithms =  Microsoft.ML.Probabilistic.Algorithms;
-
+using System.IO;
 namespace model
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            
+            // the nps.csv file contains all the customer nps scores.
+            // responseFilename = "responses-generate.csv";
+            string[] lines = File.ReadAllLines("..\\data\\iris-one-feature.csv");
+            bool[] isSetosa = new bool[lines.Length];
+            double[] featureVal = new double[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] strArray = lines[i].Split('|');
+                isSetosa[i] = strArray[1] == "1";
+                featureVal[i] = float.Parse(strArray[0].Replace(".", ","));
+            }
+
+            // Creating the model
+
+            int numberOfFlowers = lines.Length;
+            Range flower = new Range(numberOfFlowers).Named("flower");
+
+            // Make sure that the range across flowers is handled sequentially
+            // - this is necessary to ensure the model converges during training
+            flower.AddAttribute(new Sequential());
+
+            // observed data
+            VariableArray<double> featureValues = Variable.Array<double>(flower).Named("featureValue").Attrib(new DoNotInfer());
+            // FeatureValue = Variable.Array<double>(flower).Named("featureValue").Attrib(new DoNotInfer());
+
+            // The weight
+            WeightPrior = Variable.New<Gaussian>().Named("WeightPrior").Attrib(new DoNotInfer());
+            Weight = Variable.New<double>().Named("weight");
+            Weight.SetTo(Variable<double>.Random(WeightPrior));
+
+            // The threshold
+            ThresholdPrior = Variable.New<Gaussian>().Named("ThresholdPrior").Attrib(new DoNotInfer());
+            Threshold = Variable.New<double>().Named("threshold");
+            Threshold.SetTo(Variable<double>.Random(ThresholdPrior));
+
+            // Noise Variance
+            NoiseVariance = Variable.New<double>().Named("NoiseVariance").Attrib(new DoNotInfer());
+
+            // Label: is the message replied to?
+            RepliedTo = Variable.Array<bool>(message).Named("repliedTo");
+
+            // Loop over emails
+            using (Variable.ForEach(message))
+            {
+                var score = (FeatureValue[message] * Weight).Named("score");
+
+                var noisyScore = Variable.GaussianFromMeanAndVariance(score, NoiseVariance).Named("noisyScore");
+                RepliedTo[message] = noisyScore > Threshold;
+            }
+
+            // InitializeEngine();
+
+            // Engine.OptimiseForVariables = Mode == InputMode.Training
+            //                                        ? new IVariable[] { Weight, Threshold }
+            //                                        : Engine.OptimiseForVariables = new IVariable[] { RepliedTo };
+
+            
+
             Variable<bool> firstCoin = Variable.Bernoulli(0.5).Named("firstCoin");
             Variable<bool> secondCoin = Variable.Bernoulli(0.5).Named("secondCoin");
             Variable<bool> bothHeads = (firstCoin & secondCoin).Named("bothHeads");
